1a983ba943bc246228c9bd620925e51bc91958e9
sam rubarema <samrubarema6@gmail.com>
2023-05-09 17:45:30 +0300

    Made changes to URL and readme file

--- src/schema.ts
+++ src/schema.ts
@@ -3,17 +3,19 @@ import axios from "axios";
 
 const prisma = new PrismaClient();
 
+const URL = 'https://swapi.dev/api/people'
+
 export const resolvers = {
   Query: {
     allUsers: async (_: any, { page }: { page: number }) => {
       const response = await axios.get(
-        `https://swapi.dev/api/people/?page=${page}`
+        `${URL}/?page=${page}`
       );
       return response.data;
     },
     searchUser: async (_: any, { name }: { name: string }) => {
       const response = await axios.get(
-        `https://swapi.dev/api/people/?search=${name}`
+        `${URL}/?search=${name}`
       );
       return response.data.results;
     },
